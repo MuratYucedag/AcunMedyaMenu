affe7190ccb779f56fe0acaf852ed71c9228c9fd
MuratYucedag <yucedagmurat23@gmail.com>
2024-05-27 21:41:45 +0300

    Add project files.

---
+++ AcunMedyaMenu/Controllers/DefaultController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AcunMedyaMenu.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialCarousel()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialContact()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialTestimonial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialTeam()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFeature()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialAbout()
+        {
+            return PartialView();
+        }
+    }
+}
