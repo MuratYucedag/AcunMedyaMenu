3f0565e25fee325554553b0a254b07bf8094e092
MuratYucedag <yucedagmurat23@gmail.com>
2024-07-08 21:40:02 +0300

    ...

---
+++ AcunMedyaMenu/ViewComponents/DashboardViewComponents/_StatisticsDashboardComponentPartial.cs
@@ -0,0 +1,22 @@
+﻿using AcunMedyaMenu.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcunMedyaMenu.ViewComponents.DashboardViewComponents
+{
+    public class _StatisticsDashboardComponentPartial : ViewComponent
+    {
+        MenuContext context = new MenuContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.productCount = context.Products.Count();
+            ViewBag.activeProductCount = context.Products.Where(x => x.Status == true).Count();
+            ViewBag.avgProductPrice = context.Products.Average(x => x.Price);
+
+            Random rnd = new Random();
+            int count = rnd.Next(10, 31);
+
+            ViewBag.bookingCount = count;
+            return View();
+        }
+    }
+}
